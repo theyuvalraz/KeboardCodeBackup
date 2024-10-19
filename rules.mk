eb478ec164015c445321a12da0638386bcf5f958
Yuval Raz <yuvalzhe@gmail.com>
2024-10-19 15:13:56 +0300

    Add files via upload

--- rules.mk
+++ rules.mk
@@ -1,7 +1,7 @@
-SRC += muse.c
+VIA_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
 
-RGBLIGHT_ENABLE = yes
-AUDIO_ENABLE = yes
+VPATH += keyboards/keychron/common
+SRC += keychron_common.c
 AUTO_SHIFT_ENABLE = yes
-TAP_DANCE_ENABLE = yes      # Enable support for Tap Dance
-ENCODER_ENABLE = yes
+TAP_DANCE_ENABLE = yes
